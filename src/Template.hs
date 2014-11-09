ee69b101c93629c7daf1889a85d0df42e0816521
Thomas Athorne <thomas.athorne@gmail.com>
2014-11-09 18:04:08 +0000

    Get all the countries; and know how to compare them.

--- src/Template.hs
+++ src/Template.hs
@@ -15,13 +15,15 @@ data QuestionTemplate a =
   QT { description :: Text,
        query :: ByteString,
        compProp :: Traversal' Value a,
-       nameProp :: Traversal' Value Text }
+       nameProp :: Traversal' Value Text,
+       comparator :: a -> a -> Bool }
 
 instance Show (QuestionTemplate a) where
   show QT{ description = s } = show s
 
 exampleTemplate :: QuestionTemplate Scientific
 exampleTemplate = QT "Which country has a larger population?"
-                  "[{\"/location/statistical_region/population\": [{\"number\": null,\"year\": null,\"sort\": \"-year\",\"limit\": 1}], \"type\": \"/location/country\",\"name\": null}]"
-                  (values.key "/location/statistical_region/population".key "number"._Number)
+                  "[{\"/location/statistical_region/population\": [{\"number\": null,\"year\": null,\"sort\": \"-year\",\"limit\": 1}], \"type\": \"/location/country\",\"name\": null,\"limit\": 250}]"
+                  (values.key "/location/statistical_region/population".values.key "number"._Number)
                   (values.key "name"._String)
+                  (>)
