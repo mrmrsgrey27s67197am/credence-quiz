c141a1c696562613889e30330ef729311c08259b
Thomas Athorne <thomas.athorne@gmail.com>
2014-11-09 22:50:11 +0000

    New Question type and question asking.

--- src/Logic.hs
+++ src/Logic.hs
@@ -7,10 +7,13 @@ import qualified Data.Map as M
 import Control.Lens
 import Control.Applicative
 import Control.Monad.State
+import qualified Data.Text as T
+import Network.HTTP.Conduit
 
-data Question = Question String Bool
-  deriving (Show, Eq)
+data Answer = A | B deriving (Show, Eq)
 
+data Question = Question { qText, aText, bText :: T.Text,
+                           answer :: Answer }
   deriving (Show, Eq)
 
 data GameState = GameState { _answers :: Map Int Int,
@@ -19,16 +22,32 @@ data GameState = GameState { _answers :: Map Int Int,
 makeLenses ''GameState
 
 newtype Game a = Game (StateT GameState IO a)
-  deriving (Functor, Applicative, Monad, MonadState GameState)
+  deriving (Functor, Applicative, Monad, MonadState GameState, MonadIO)
 
 initial :: GameState
 initial = undefined & answers .~ M.empty
 
 question :: Question -> Int -> Game ()
-question (Question _ answer) credence =
-  if answer
-  then answers.at credence.non 0 += 1
-  else answers.at (100 - credence).non 0 += 1
+question (Question q a b answer) credence = do
+  userAnswer <- liftIO $ do
+    putStrLn $ T.unpack q
+    putStrLn $ "A) " ++ T.unpack a
+    putStrLn $ "B) " ++ T.unpack b
+    getUserAnswer
+  if answer == userAnswer
+    then answers.at credence.non 0 += 1
+    else answers.at (100 - credence).non 0 += 1
+
+getUserAnswer :: IO Answer
+getUserAnswer = do
+  s <- getLine
+  case s of
+    "A" -> return A
+    "B" -> return B
+    "a" -> return A
+    "b" -> return B
+    _ -> do putStrLn "Don't understand, please try again."
+            getUserAnswer
 
 countScore :: Map Int Int -> Double
 countScore m = let l = M.toList m
