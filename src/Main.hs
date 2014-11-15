f55d615f2b120ea16f374a8d04f8bbf6d38588b5
Thomas Athorne <thomas.athorne@gmail.com>
2014-11-15 16:53:48 +0000

    'initiateManager' and 'runGame'.

--- src/Main.hs
+++ src/Main.hs
@@ -5,5 +5,19 @@ import Logic
 import Freebase
 import Template
 
+import Control.Monad.State
+import Control.Lens
+
 main :: IO ()
 main = return ()
+
+initiateManager :: Game ()
+initiateManager = do
+  man <- liftIO getManager
+  manager .= man
+
+
+runGame :: Game a -> IO (a, GameState)
+runGame g = let g' = do initiateManager
+                        g
+            in runStateT (game g') initial
