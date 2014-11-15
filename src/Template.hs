76fd5140d5b16380394de50e6484236098c2bb15
Thomas Athorne <thomas.athorne@gmail.com>
2014-11-15 16:53:03 +0000

    Finished 'generateQuestion'.

--- src/Template.hs
+++ src/Template.hs
@@ -11,6 +11,7 @@ import Data.Aeson.Types
 import Data.Scientific
 import Data.ByteString.Internal
 import System.Random
+import Control.Monad.State
 
 import Freebase
 import Logic
@@ -34,16 +35,21 @@ chooseTwo as = do
     else return (as !! pred a, as !! pred b)
 
 deMaybe :: (Maybe a, Maybe b, Maybe c, Maybe d) -> Maybe (a, b, c, d)
-deMaybe = undefined
+deMaybe (ma, mb, mc, md) = do
+  a <- ma; b <- mb; c <- mc; d <- md;
+  return (a,b,c,d)
 
 generateQuestion :: QuestionTemplate a -> Game Question
 generateQuestion qt = do
   resp <- queryFreebase $ query qt
-  (a,b) <- chooseTwo $ resp ^.. _Just.values
-  case deMaybe $ (a^?compProp qt, a^?nameProp qt,
-                  b^?compProp qt, b^?nameProp qt) of
+  (a,b) <- liftIO $ chooseTwo $ resp ^.. _Just.values
+  case deMaybe (a^?compProp qt, a^?nameProp qt,
+                b^?compProp qt, b^?nameProp qt) of
     Nothing -> generateQuestion qt
-    Just (aVal, aName, bVal, bName) -> undefined
+    Just (aVal, aName, bVal, bName) -> return $
+      Question (description qt) aName bName $ case (comparator qt) aVal bVal of
+        True -> A
+        False -> B
 
 
 exampleTemplate :: QuestionTemplate Scientific
